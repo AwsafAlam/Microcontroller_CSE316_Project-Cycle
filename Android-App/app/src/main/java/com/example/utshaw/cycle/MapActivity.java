47950abcc5dec181845e559d133698472b21f2d4
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-06-06 17:59:40 +0600

    Added QR code scanner

--- Android-App/app/src/main/java/com/example/utshaw/cycle/MapActivity.java
+++ Android-App/app/src/main/java/com/example/utshaw/cycle/MapActivity.java
@@ -1,16 +1,21 @@
 package com.example.utshaw.cycle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
@@ -23,14 +28,16 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
+        NavigationView.OnNavigationItemSelectedListener{
 
     private GoogleMap mMap;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
     private BottomSheetBehavior sheetBehavior;
-//    private View layoutBottomSheet;
+    private Button btn;
+    private TextView textView;
 
 
     @Override
@@ -38,6 +45,18 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        String barcode = getIntent().getStringExtra("code");
+
+        textView = findViewById(R.id.barcode);
+        if (TextUtils.isEmpty(barcode)) {
+            //Toast.makeText(getApplicationContext(), "Barcode is empty!", Toast.LENGTH_LONG).show();
+            //finish();
+            textView.setText("No Code");
+        }
+        else{
+            textView.setText(barcode);
+            sendtoGSM(barcode);
+        }
 
         mToolbar = findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
@@ -46,12 +65,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 //        actionBar.hide();
 //        actionBar.setDisplayHomeAsUpEnabled(true);
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         mDrawerLayout = findViewById(R.id.drawerlayout);
         mToggle = new ActionBarDrawerToggle(this , mDrawerLayout , R.string.Open_nav,R.string.Close_nav);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        btn = findViewById(R.id.unlock);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(this , "Clicked acc" , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MapActivity.this, ScannerActivity.class));
+
+            }
+        });
+
 
 //        layoutBottomSheet = findViewById(R.id.bottom_sheet);
 
@@ -127,21 +160,52 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         });
     }
 
+    private void sendtoGSM(String barcode) {
+
+        String phoneNumber = "+8801556358935";
+        String message = "Sending Test Message : "+barcode;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+        intent.putExtra("sms_body", message);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if(mToggle.onOptionsItemSelected(item)){
-            return true;
+            int id = item.getItemId();
+
+            if(id == R.id.user){
+                Toast.makeText(this , "Clicked acc" , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MapActivity.this, ScannerActivity.class));
+                return true;
+
+            }
+            else if(id == R.id.places){
+                return true;
+            }
         }
+
         return super.onOptionsItemSelected(item);
     }
 
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.timeline){
+            Toast.makeText(this , "Clicked timeLine---" , Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MapActivity.this, ScannerActivity.class));
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
     }
-
-
 }
---
+++ Android-App/app/src/main/java/com/example/utshaw/cycle/ScannerActivity.java
@@ -0,0 +1,60 @@
+package com.example.utshaw.cycle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+public class ScannerActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    BarcodeReader barcodeReader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner2);
+
+        // get the barcode reader instance
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
+
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+        // playing barcode reader beep sound
+        barcodeReader.playBeep();
+//        Toast.makeText(getApplicationContext(), "CODE:- " + barcode.displayValue, Toast.LENGTH_LONG).show();
+
+        // ticket details activity by passing barcode
+        Intent intent = new Intent(ScannerActivity.this, MapActivity.class);
+        intent.putExtra("code", barcode.displayValue);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+        Toast.makeText(getApplicationContext(), "Error occurred while scanning " + errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        finish();
+    }
+}
