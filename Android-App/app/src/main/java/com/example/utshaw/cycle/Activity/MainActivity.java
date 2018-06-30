b73f9418b3afb913bc8258f6675f2d12d4f98dcb
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-07-01 02:33:10 +0600

    Added Signup Login screen

--- Android-App/app/src/main/java/com/example/utshaw/cycle/Activity/MainActivity.java
+++ Android-App/app/src/main/java/com/example/utshaw/cycle/Activity/MainActivity.java
@@ -1,62 +1,141 @@
 package com.example.utshaw.cycle.Activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.utshaw.cycle.R;
 import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
-//    public static final int RC_SIGN_IN = 1;
-//
-//    private String userAuthPhoneNumber;
-//
-//    private FirebaseAuth mFirebaseAuth;
-//    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
-//
-//        mFirebaseAuth = FirebaseAuth.getInstance();
-//
-//        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                FirebaseUser user = mFirebaseAuth.getCurrentUser();
-//                if(user != null){
-//                    // user is signed in
-//                    onSignedInInitalize();
-//                }else{
-//                    // user is logged out
-//                    startActivityForResult(
-//                            AuthUI.getInstance()
-//                                    .createSignInIntentBuilder()
-//                                    .setAvailableProviders(
-//                                            Arrays.asList(
-//                                                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()))
-//                                    .build(),
-//                            RC_SIGN_IN);
-//                }
-//
-//            }
-//        };
-//
 
+        final SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+        String loggedIn = sharedPreferences.getString("loggedIn", "false");
+
+        if(loggedIn.equals("true")){
+            Intent main_activity = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(main_activity);
+            finish();
+        }
+
+
+        final TextView goto_signup_text = (TextView) findViewById(R.id.textView_login_signup_button);
+
+        goto_signup_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup_page = new Intent(getApplicationContext(),Signup_form_one.class);
+                startActivity(signup_page);
+                finish();
+            }
+
+        });
+
+
+
+        final TextView goto_signup_text2 = (TextView) findViewById(R.id.textView);
+
+        goto_signup_text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signup_page = new Intent(getApplicationContext(),Signup_form_one.class);
+                startActivity(signup_page);
+                finish();
+
+            }
+
+        });
+
+
+
+        final Button login_button = (Button) findViewById(R.id.button_signin);
+
+        final EditText login_username = (EditText) findViewById(R.id.editText_login_name);
+        final EditText login_mobile = (EditText) findViewById(R.id.editText_login_mobile);
+
+
+
+        login_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String rightusername = sharedPreferences.getString("userName", "");
+                        String rightmobile = sharedPreferences.getString("userMobile", "");
+                        String given_username = login_username.getText().toString();
+                        String given_mobile = login_mobile.getText().toString();
+
+                        if(rightmobile.equals("") || rightusername.equals("")){
+                            Toast.makeText(getApplicationContext(), "No user account available. Please signup.", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            if(given_mobile.equals(rightmobile) && given_username.equals(rightusername)){
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("loggedIn", "true");
+                                editor.apply();
+                                Intent main_activity = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(main_activity);
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Wrong Username or Mobile No", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        startActivity(new Intent(MainActivity.this, MapActivity.class));
+
+                    }
+                }
+        );
 
 
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+        a_builder.setMessage("Do you want to Exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }) ;
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Exit Confirmation");
+        alert.show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -111,7 +190,7 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 
-    public void goToMap(View view) {
-        startActivity(new Intent(MainActivity.this, MapActivity.class));
-    }
+//    public void goToMap(View view) {
+//        startActivity(new Intent(MainActivity.this, MapActivity.class));
+//    }
 }
---
+++ Android-App/app/src/main/java/com/example/utshaw/cycle/Activity/Signup_form_two.java
@@ -0,0 +1,232 @@
+package com.example.utshaw.cycle.Activity;
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.utshaw.cycle.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class Signup_form_two extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 1;
+
+    private String userAuthPhoneNumber;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.signupAppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_form_two);
+
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        Button sign_up_finish_button = (Button) findViewById(R.id.button_signup_finish);
+
+        final EditText signUp_age = (EditText) findViewById(R.id.signupAge);
+        final EditText signUp_gender = (EditText) findViewById(R.id.signupGender);
+        final EditText signUp_bloodgrp = (EditText) findViewById(R.id.signupBlood);
+        final EditText signUp_address = (EditText) findViewById(R.id.signupAddress);
+
+        final TextView genderText = (TextView) findViewById(R.id.textView12);
+        final TextView addressText = (TextView) findViewById(R.id.textView14);
+
+        final TextView goto_login_text = (TextView) findViewById(R.id.textView_signupform_login_button2);
+
+        goto_login_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(Signup_form_two.this);
+                a_builder.setMessage("Do you want to go the Login Page?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                final SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("userName","");
+                                editor.putString("userMobile", "");
+                                editor.putString("userAge", "");
+                                editor.putString("userGender", "");
+                                editor.putString("userBloodGroup", "");
+                                editor.putString("userAddress", "");
+                                editor.apply();
+                                Intent loginPage = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(loginPage);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }) ;
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Login Page Confirmation");
+                alert.show();
+            }
+        });
+
+        final TextView goto_login_text2 = (TextView) findViewById(R.id.textView3);
+
+        goto_login_text2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(Signup_form_two.this);
+                a_builder.setMessage("Do you want to go the Login Page?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                final SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("userName","");
+                                editor.putString("userMobile", "");
+                                editor.putString("userAge", "");
+                                editor.putString("userGender", "");
+                                editor.putString("userBloodGroup", "");
+                                editor.putString("userAddress", "");
+                                editor.putString("loggedIn", "false");
+                                editor.apply();
+                                Intent loginPage = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(loginPage);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }) ;
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Login Page Confirmation");
+                alert.show();
+            }
+        });
+
+
+        sign_up_finish_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String given_age = signUp_age.getText().toString();
+                        String given_gender = signUp_gender.getText().toString();
+                        String given_bloodgrp = signUp_bloodgrp.getText().toString();
+                        String given_address = signUp_address.getText().toString();
+                        if(given_gender.equals("") || given_address.equals("")){
+                            genderText.setText("*GENDER");
+                            addressText.setText("*ADDRESS");
+                            Toast.makeText(getApplicationContext(), "Please fill up the mandatory fields", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            final SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("userAge", given_age);
+                            editor.putString("userGender", given_gender);
+                            editor.putString("userBloodGroup", given_bloodgrp);
+                            editor.putString("userAddress", given_address);
+                            editor.putString("loggedIn", "false");
+                            editor.apply();
+
+                        }
+
+
+                        mFirebaseAuth = FirebaseAuth.getInstance();
+
+                        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+                            @Override
+                            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                                if(user != null){
+                                    // user is signed in
+                                    onSignedInInitalize();
+                                }else{
+                                    // user is logged out
+                                    startActivityForResult(
+                                            AuthUI.getInstance()
+                                                    .createSignInIntentBuilder()
+                                                    .setAvailableProviders(
+                                                            Arrays.asList(
+                                                                    new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()))
+                                                    .build(),
+                                            RC_SIGN_IN);
+                                }
+
+                            }
+                        };
+                        Intent loginPage = new Intent(getApplicationContext(),MainActivity.class);
+
+                        startActivity(loginPage);
+                        finish();
+                    }
+                }
+        );
+    }
+
+
+    private void onSignedInInitalize(){
+        userAuthPhoneNumber = mFirebaseAuth.getCurrentUser().getPhoneNumber().toString();
+        Toast.makeText(this, "Hello " + userAuthPhoneNumber, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        setTheme(R.style.signupAppTheme);
+        super.onRestart();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(Signup_form_two.this);
+        a_builder.setMessage("Do you want to go the Login Page?")
+                .setCancelable(false)
+                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final SharedPreferences sharedPreferences = getSharedPreferences("signUpInfo", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("userName","");
+                        editor.putString("userMobile", "");
+                        editor.putString("userAge", "");
+                        editor.putString("userGender", "");
+                        editor.putString("userBloodGroup", "");
+                        editor.putString("userAddress", "");
+                        editor.apply();
+                        Intent loginPage = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(loginPage);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }) ;
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Login Page Confirmation");
+        alert.show();
+    }
+}
