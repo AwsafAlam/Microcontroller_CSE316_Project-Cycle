512360dc1c4463e5ab2b6ba90f91be4aa9d37689
Utshaw <farhan.tanvir.utshaw@gmail.com>
2018-07-11 16:38:00 +0600

    Add Servo motor code

---
+++ ATmega_Codes/Servo/servo.c
@@ -0,0 +1,28 @@
+#ifndef F_CPU
+#define F_CPU 8000000UL // 1 MHz clock speed
+#endif
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+
+int main(void)
+{
+	DDRD = 0XFF;
+	TCCR1A |= 1 << WGM11 | 1 << COM1A1 | 1 << COM1A0;
+	TCCR1B |= 1<<WGM12 | 1<<WGM13 | 1<<CS10;
+	ICR1 = 19999;
+
+	
+
+	while(1){
+		OCR1A = ICR1 - 800;
+		_delay_ms(100);
+		OCR1A = ICR1 - 2200;
+		_delay_ms(100);
+	}
+	
+}
