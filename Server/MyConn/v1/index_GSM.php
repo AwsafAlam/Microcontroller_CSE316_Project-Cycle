6368f81c74fd83e4fe83e63b5e574801075553f2
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-07-15 02:36:02 +0600

    GSM server

--- Server/MyConn/v1/index_GSM.php
+++ Server/MyConn/v1/index_GSM.php
@@ -9,45 +9,6 @@ require_once __DIR__.'/src/Facebook/autoload.php';
 
 $app = new \Slim\Slim();
 
-
-$user_id = NULL;
-// echo "lock";
-
-function authenticate(\Slim\Route $route)
-{
-    // Getting request headers
-    $headers = apache_request_headers();
-    $response = array();
-    $app = \Slim\Slim::getInstance();
-
-    // Verifying Authorization Header
-    if (isset($headers['Authorization'])) {
-        $db = new DbHandler();
-
-        // get the api key
-        $api_key = $headers['Authorization'];
-        // validating api key
-        if (!$db->isValidApiKey($api_key)) {
-            // api key is not present in users table
-            $response["error"] = true;
-            $response["message"] = "Access Denied. Invalid Api key";
-            echoRespnse(401, $response);
-            $app->stop();
-        } else {
-            global $user_id;
-            // get user primary key id
-            $user_id = $db->getUserId($api_key);
-        }
-    } else {
-        // api key is missing in header
-        $response["error"] = true;
-        $response["message"] = "Api key is misssing";
-        echoRespnse(400, $response);
-        $app->stop();
-    }
-}
-
-
 $app->get('/updateloc', function() use ($app)  {
 
 	$Latitude = $_GET['lat'];
@@ -86,13 +47,11 @@ $app->get('/updateloc', function() use ($app)  {
 	   $result->close();
 
 
-
-  echoRespnse(201,$posts);
+echo $state;
+  //echoRespnse(201,$posts);
 
 
 });
-
-
 
 function echoRespnse($status_code, $response)
 {
--- developers.onlinesohopathi.com/README.md
+++
@@ -1,1 +0,0 @@
-# developers.onlinesohopathi.com
