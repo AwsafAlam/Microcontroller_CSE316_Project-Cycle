2761850bb941a2f440b406f85a2e53d6913944b5
AwsafAlam <1505114.maaa@ugead.cse.buet.ac.bd>
2018-07-17 19:47:39 +0600

    Added bluetooth and servo code

---
+++ ATmega_Codes/Bluetooth_USART_SERVO/Bluetooth_USART_SERVO/main.c
@@ -0,0 +1,55 @@
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "Servo.h"
+#include "UART.h"
+
+
+int main(void)
+{
+	DDRA = 0x0F;
+	DDRD = 0XFF;
+	PORTA = 0xFF;
+	
+	ServoInit();
+	UART_init(9600);
+	
+	sei();
+	
+	while (1)
+	{
+		
+	}
+	
+}
+
+ISR (USART_RXC_vect)
+{
+	uint8_t oldsrg = SREG;
+	
+	cli();
+	char data = UDR;
+	PORTA = 0xF0;
+	
+	if(data=='1'){
+		PORTA |= (1<<PORTA0);
+		debugClockWise();
+		UART_send(data) ;
+	}
+	else if(data=='0'){
+		PORTA |= (1<<PORTA1);
+		debugAntiClockWise();
+		UART_send(data) ;
+	}
+	else{
+		PORTA |= (1<<PORTA2);
+		UART_send(data) ;
+	}
+	
+	SREG = oldsrg;
+}
