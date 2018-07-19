db01405ffd4a35fa26d28afbe520866c0d0833cf
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-07-19 19:45:36 +0600

    Added GPS and ADC_ code:

---
+++ Arduino_Codes/GPS_Working/GPS_Working.ino
@@ -0,0 +1,90 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("uBlox Neo 6M Test");
+  Serial1.begin(GPSBaud);
+}
+
+void loop() // run over and over
+{
+  if (Serial1.available()){
+   //  Serial.write(Serial1.read());
+
+   //Serial.println("Data");
+    gps.encode(Serial1.read());
+    if (gps.location.isUpdated()){
+      Serial.println("------------------------------------------------------");
+      // Latitude in degrees (double)
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);      
+      // Longitude in degrees (double)
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6); 
+       
+      // Raw latitude in whole degrees
+      Serial.print("Raw latitude = "); 
+      Serial.print(gps.location.rawLat().negative ? "-" : "+");
+      Serial.println(gps.location.rawLat().deg); 
+      // ... and billionths (u16/u32)
+      Serial.println(gps.location.rawLat().billionths);
+      
+      // Raw longitude in whole degrees
+      Serial.print("Raw longitude = "); 
+      Serial.print(gps.location.rawLng().negative ? "-" : "+");
+      Serial.println(gps.location.rawLng().deg); 
+      // ... and billionths (u16/u32)
+      Serial.println(gps.location.rawLng().billionths);
+
+      // Year (2000+) (u16)
+      Serial.print("Year = "); 
+      Serial.println(gps.date.year()); 
+      // Month (1-12) (u8)
+      Serial.print("Month = "); 
+      Serial.println(gps.date.month()); 
+      // Day (1-31) (u8)
+      Serial.print("Day = "); 
+      Serial.println(gps.date.day()); 
+
+      // Raw time in HHMMSSCC format (u32)
+      Serial.print("Raw time in HHMMSSCC = "); 
+      Serial.println(gps.time.value()); 
+
+      // Hour (0-23) (u8)
+      Serial.print("Hour = "); 
+      Serial.println(gps.time.hour()); 
+      // Minute (0-59) (u8)
+      Serial.print("Minute = "); 
+      Serial.println(gps.time.minute()); 
+      // Second (0-59) (u8)
+      Serial.print("Second = "); 
+      Serial.println(gps.time.second()); 
+      
+      
+      // Speed in meters per second (double)
+      Serial.print("Speed in m/s = ");
+      Serial.println(gps.speed.mps()); 
+
+      // Altitude in feet (double)
+      Serial.print("Altitude in feet = "); 
+      Serial.println(gps.altitude.feet()); 
+
+      // Number of satellites in use (u32)
+      Serial.print("Number os satellites in use = "); 
+      Serial.println(gps.satellites.value()); 
+      Serial.println("------------------------------------------------------");
+      
+      delay(1000);
+      
+    }
+    }
+}
+
